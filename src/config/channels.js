9b8737b29f9dc39fcb36d66de78d4a049a0f3f08
predator522 <p47017988@gmail.com>
2026-09-07 13:41:42 +0000

    Create channels.js

---
+++ src/config/channels.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    username: "@theentity7",
+    title: "Channel One",
+    url: "https://t.me/theentity7"
+  },
+  {
+    username: "@FRANXXVIP",
+    title: "Channel Two",
+    url: "https://t.me/FRANXXVIP"
+  },
+  {
+    username: "@ENTITYCYBERTERMINAL",
+    title: "Channel Three",
+    url: "https://t.me/ENTITYCYBERTERMINAL"
+  }
+];
